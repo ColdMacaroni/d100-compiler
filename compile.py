f9ee95c36cd0ca7995a7f4f01a9c48b35d60ee5a
muffin <diegoxe7117@gmail.com>
2021-12-31 01:39:26 +1300

    Reading table, ignores empty and commented lines

--- compile.py
+++ compile.py
@@ -5,6 +5,11 @@
 
 import sys
 
+#---------#
+# GLOBALS #
+#---------#
+COMMENT_CHAR = '#'
+
 # TODO: Convert to C program
 # TODO: Do this by going through directory and sub directories.
 # TODO: BIGBOI: Implement tests.
@@ -33,12 +38,52 @@ class DiceTable:
         self.tables = tables
 
 
+def parse_tab(text: list[str]) -> DiceTable:
+    """
+    Parses the contents of a .tab file into a DiceTable object.
+    Does NOT read the file, takes the contents as a list of strings.
+    Runs each line through .strip()
+    """
+    # Get rid of extra spaces and new lines for easier handling.
+
+    text = [l.strip() for l in text]
+
+    # We need to get 3 things:
+    #   Table name, Dice needed, The tables for each die.
+    table = DiceTable()
+
+    for line in text:
+        # Skip commented out lines and empty lines.
+        #
+        # Empty line check first so that lazy evaluation saves us from
+        # IndexError on the second condition.
+        if not line or line[0] == COMMENT_CHAR:
+            continue
+
+        print(line)
+
+    return table
+
+
 def main(files: list[str]):
     """
     Goes through the files passed as arguments and parses each tab file.
     Then it creates a corresponding C program and compiles it.
     """
-    ...
+    tables = list()
+
+    for fn in files:
+        with open(fn, "r") as file:
+            # Preemptively remove any spaces and new lines to make everything
+            # easier later on
+            text = file.readlines()
+
+        table = parse_tab(text)
+        tables.append(table)
+
+    return tables
+
+
 
 if __name__ == "__main__":
     assert len(sys.argv) > 1, "Needs at least one file or directory as argument."
